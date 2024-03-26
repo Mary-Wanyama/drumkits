0e1a964015bf836f4035ecd5249d01ce8069f694
Mary-Wanyama <immaculatemariah@gmail.com>
2024-03-27 00:41:24 +0300

    letter

--- script.js
+++ script.js
@@ -17,32 +17,32 @@ document.addEventListener('keypress', function(event){
 
 function makeSound(key) {
     switch (key) {
-        case 'W':
+        case 'A':
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
-            case 'A':
+            case 'S':
                 var tom2 = new Audio("sounds/tom-2.mp3");
                 tom2.play();
             break;
-            case 'S':
+            case 'D':
                 var tom3 = new Audio("sounds/tom-3.mp3");
                 tom3.play();
             break;
-            case 'D':
+            case 'F':
                 var tom4 = new Audio("sounds/tom-4.mp3");
                 tom4.play();
             break;
             case 'J':
-                var tom5 = new Audio("sounds/tom-1.mp3");
+                var tom5 = new Audio("sounds/crash.mp3");
                 tom5.play();
             break;
             case 'K':
-                var tom6 = new Audio("sounds/tom-2.mp3");
+                var tom6 = new Audio("sounds/kick-bass.mp3");
                 tom6.play();
             break;
             case 'L':
-                var tom7 = new Audio("sounds/tom-3.mp3");
+                var tom7 = new Audio("sounds/snare.mp3");
                 tom7.play();
             break;
     
